e8e3f2c58f5cc79c556c098732a88946cdd5d39d
何子勤 <tzh005@ucsd.edu>
2025-08-03 16:28:42 +0800

    Initial Commit

---
+++ sensorCollect/sensorCollectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sensorCollectApp.swift
+//  sensorCollect
+//
+//  Created by 何子勤 on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct sensorCollectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sensorCollectTests/sensorCollectTests.swift
@@ -0,0 +1,17 @@
+//
+//  sensorCollectTests.swift
+//  sensorCollectTests
+//
+//  Created by 何子勤 on 8/3/25.
+//
+
+import Testing
+@testable import sensorCollect
+
+struct sensorCollectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
